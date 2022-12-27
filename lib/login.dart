55a01d1ea6a076eb683f1b1cc355cc6a997c7f3c
Sauter001 <lsj457@naver.com>
2022-12-27 17:48:49 +0900

    second commit: 주석 추가

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  /* TODO: DB 연결 테스트 화면임 로그인 화면으로 수정 필요 */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
