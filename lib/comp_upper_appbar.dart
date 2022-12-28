171155d6e4a832d24e9815f074c1025600cbab35
Sauter001 <lsj457@naver.com>
2022-12-28 16:01:19 +0900

    chore: web 폴더 추가 docs: pubspec.yaml 수정됨 feat: upper appbar changeTitle() method 추가 feat: mypage.dart 프로필 정보 추가 feat: login.dart 임시 DB 테스트 버튼 추가
    changeTitle() 메소드는 상단바 component의 title을 변경한다.
    login.dart의 'create' 버튼으로 C 테스트 가능

--- lib/comp_upper_appbar.dart
+++ lib/comp_upper_appbar.dart
@@ -13,12 +13,15 @@ void _openMenu() {
 
 // 상단바 구현
 class UpperAppbar {
-  final AppBar _appBar = AppBar(
+  AppBar _appBar = AppBar(
     leading: const IconButton(
       onPressed: _openMenu,
       icon: Icon(Icons.menu),
     ),
-    title: const Text('appbar title'),
+    title: const Text(
+      'appbar title',
+      textAlign: TextAlign.center,
+    ),
     actions: <Widget>[
       Container(
         decoration: BoxDecoration(
@@ -31,6 +34,36 @@ class UpperAppbar {
       ),
     ],
   );
+
+  /* 상단바 component의 title을 변경한다. */
+  UpperAppbar changeTitle(String title) {
+    _appBar = AppBar(
+      leading: const IconButton(
+        onPressed: _openMenu,
+        icon: Icon(Icons.menu),
+      ),
+      title: Text(title),
+      actions: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: IconButton(
+            onPressed: _gotoMyPage,
+            icon: ClipRRect(
+              borderRadius: BorderRadius.circular(80.0),
+              child: Image.asset(
+                'assets/images/baseProfile.jpg',
+                width: 100,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    return this;
+  }
 
   get appBar {
     return _appBar;
